b4a5893b1fef23dea867171e7ef413e4661ddc9a
rac <ricardo.rac@gmail.com>
2021-04-27 12:52:01 -0300

    first try

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     currentYear = new Date().getFullYear()
     finalYear = currentYear + years
     amount = (p * rate * years / 100)
-    out = "If you deposit " + p + ",<br> at an interest rate of " + rate + "%.<br>You will receive an amount of " + amount + ",<br>in the year " + finalYear
+    out = "If you deposit <span class='highlight'>" + p + "</span>,<br> at an interest rate of <span class='highlight'>" + rate + "%</span>.<br>You will receive an amount of <span class='highlight'>" + amount + "</span>,<br>in the year <span class='highlight'>" + finalYear + "</span>"
     console.log(out)
     document.getElementById("calcOutput").innerHTML=out
 }
