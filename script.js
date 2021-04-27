10c611d91c8dcc777c8e10ee34e08ca915fbec43
rac <ricardo.rac@gmail.com>
2021-04-27 15:25:51 -0300

    first try

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    years = document.getElementById("years").value;
-    rate = document.getElementById("rate").value;
-    currentYear = new Date().getFullYear()
-    finalYear = currentYear + years
-    amount = (p * rate * years / 100)
-    out = "If you deposit <span class='highlight'>" + p + "</span>,<br> at an interest rate of <span class='highlight'>" + rate + "%</span>.<br>You will receive an amount of <span class='highlight'>" + amount + "</span>,<br>in the year <span class='highlight'>" + finalYear + "</span>"
-    console.log(out)
+    var p = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+    var currentYear = new Date().getFullYear()
+    var finalYear = currentYear + parseInt(years)
+    var amount = (p * rate * years / 100)
+    var out = "If you deposit <span class='highlight'>" + p + "</span>,<br> at an interest rate of <span class='highlight'>" + rate + "%</span>.<br>You will receive an amount of <span class='highlight'>" + amount + "</span>,<br>in the year <span class='highlight'>" + finalYear + "</span>"
     document.getElementById("calcOutput").innerHTML=out
 }
         
